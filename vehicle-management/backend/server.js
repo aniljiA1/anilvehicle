4217979f63694b22ba9c0a2886f64fda8935b2cd
Anil Kumar <126413189+aniljiA1@users.noreply.github.com>
2026-06-05 13:51:00 +0530

    Update server.js

--- vehicle-management/backend/server.js
+++ vehicle-management/backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://anilvehicle.netlify.app"],
+    origin: ["http://localhost:5173", "https://anilvehicles.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
